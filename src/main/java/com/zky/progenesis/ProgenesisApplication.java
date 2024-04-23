def3c4ca1337eb8d6d4c9d84455cb5970a0b81fe
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-04-23 16:42:51 +0800

    Initial project

---
+++ src/main/java/com/zky/progenesis/ProgenesisApplication.java
@@ -0,0 +1,13 @@
+package com.zky.progenesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgenesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgenesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zky/progenesis/ProgenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zky.progenesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgenesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
