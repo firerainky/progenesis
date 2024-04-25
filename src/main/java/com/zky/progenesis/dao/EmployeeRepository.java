bbadae4697aa71f669fb922bc9fa29f84a1ada79
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-04-25 10:51:12 +0800

    Add Spring Data REST dependency and configs

--- src/main/java/com/zky/progenesis/dao/EmployeeRepository.java
+++ src/main/java/com/zky/progenesis/dao/EmployeeRepository.java
@@ -1,9 +1,20 @@
 package com.zky.progenesis.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.zky.progenesis.entity.Employee;
 
+// With Spring Data REST, we don't need to write any implementation code or boilerplate code
+// We already get REST API /employees, GET / POST / PUT / DELETE...
+// In such generated API, id is only added in url, not in request body
+// And if can not find an employee, it will return success status 204 No Content
+// The default endpoint path is followed simple pluralized form of the entity class name: Employee -> /employees
+// Its properties in application.properties are started with spring.data.rest.*
+// Sort by first name, descending: http://localhost:8080/api/employees?sort=firstName,desc
+// Sort by last name, then first name, ascending: http://localhost:8080/api/employees?sort=lastName,firstName
+
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     
     // that's it ... no need to write any code LOL!
