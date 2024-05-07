9d020989662cf0260ef7b6d1f2a1cb9580c488f4
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-05-07 10:46:27 +0800

    Add data and schema for authorities and users tables

--- src/main/resources/db/data-h2.sql
+++ src/main/resources/db/data-h2.sql
@@ -4,3 +4,26 @@ INSERT INTO `employee` (first_name, last_name, email) VALUES
    ('Avani','Gupta','avani@luv2code.com'),
    ('Yuri','Petrov','yuri@luv2code.com'),
    ('Juan','Vega','juan@luv2code.com');
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('john','ROLE_EMPLOYEE'),
+('mary','ROLE_EMPLOYEE'),
+('mary','ROLE_MANAGER'),
+('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_MANAGER'),
+('susan','ROLE_ADMIN');
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users` 
+VALUES 
+('john','{noop}test123',1),
+('mary','{noop}test123',1),
+('susan','{noop}test123',1);
--- src/main/resources/db/schema-h2.sql
+++ src/main/resources/db/schema-h2.sql
@@ -7,3 +7,29 @@ CREATE TABLE `employee` (
     `email` varchar(45) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
+);
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+);
+
