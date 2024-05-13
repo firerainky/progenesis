955525b8091abcb3bb53d485cb6629695dc0b30f
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-05-13 14:54:31 +0800

    Add Thymeleaf dependency and disable security configuration

--- src/main/java/com/zky/progenesis/security/DemoSecurityConfig.java
+++ src/main/java/com/zky/progenesis/security/DemoSecurityConfig.java
@@ -1,47 +1,47 @@
 package com.zky.progenesis.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
     // Add support for JDBC ... no more hard-coded users
-    @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        return userDetailsManager;
-    }
+    // @Bean
+    // public UserDetailsManager userDetailsManager(DataSource dataSource) {
+    //     JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+    //     return userDetailsManager;
+    // }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(configurer ->
-                configurer
-                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-            );
-
-        // Use HTTP Basic authentication
-        http.httpBasic(Customizer.withDefaults());
-
-        // Disable Cross Site Request Forgery (CSRF)
-        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and / or PATCH
-        http.csrf(csrf -> csrf.disable());
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         return http.build();
     }
+
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .authorizeHttpRequests(configurer ->
+    //             configurer
+    //             .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+    //             .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+    //             .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+    //             .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+    //             .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+    //         );
+
+    //     // Use HTTP Basic authentication
+    //     http.httpBasic(Customizer.withDefaults());
+
+    //     // Disable Cross Site Request Forgery (CSRF)
+    //     // in general, not required for stateless REST APIs that use POST, PUT, DELETE and / or PATCH
+    //     http.csrf(csrf -> csrf.disable());
+
+    //     return http.build();
+    // }
 
     // @Bean
     // public InMemoryUserDetailsManager userDetailsManager() {
