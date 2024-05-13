7684a77285af1111f4b7937d7b18ff01fd7dcc64
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-05-13 16:40:24 +0800

    Add new methods to HelloWorldController and update form action

--- src/main/java/com/zky/progenesis/controller/HelloWorldController.java
+++ src/main/java/com/zky/progenesis/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package com.zky.progenesis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 
 
 
@@ -15,6 +20,23 @@ public class HelloWorldController {
     
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String theName = request.getParameter("studentName");
+        theName = theName.toUpperCase();
+        String result = "Yo! " + theName;
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+        theName = theName.toUpperCase();
+        String result = "Hey My Friend from V3! " + theName;
+        model.addAttribute("message", result);
         return "helloworld";
     }
 }
